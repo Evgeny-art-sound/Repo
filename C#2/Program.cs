f1fcde9eb9233f2be6e0cbc583663b92ec387d94
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-04-25 14:34:19 +0500

    решали задачи

---
+++ C#2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("input digit A ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("input digit B ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("input digit C ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > a)
+{
+   max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("max = ");
+Console.WriteLine(max);
---
+++ C#3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("input digit ");
+int a = int.Parse(Console.ReadLine());
+int b = -a;
+
+while (b<=a)
+{
+    Console.WriteLine(b);
+    b = b+1;
+}
