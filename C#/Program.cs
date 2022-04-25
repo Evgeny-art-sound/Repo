f851589b8ef69bc3538087776ecb0c5810db45ae
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-04-25 13:12:41 +0500

    Add C#

---
+++ C#/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("input digit A ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("input digit B ");
+int b = int.Parse(Console.ReadLine());
+if (a != b)
+{
+
+
+    if (a > b)
+    {
+        Console.WriteLine("А > B");
+
+    }
+    else
+    {
+        Console.WriteLine("B > A");
+    }
+}
+else
+{
+    Console.WriteLine("числа равны");
+}
